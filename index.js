a8f39fac18676e38f93eefeb0fc025da61ac8e62
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-06-06 20:49:40 +0200

    fixed initialization

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var methods = require('./lib/methods');
 module.exports = (function() {
   Object.keys(methods).forEach(function(method) {
     Object.defineProperty(Array.prototype, method, {
-      enumerable: false,
-      get: methods[method]
+      value: methods[method]
     });
   });
 })();
