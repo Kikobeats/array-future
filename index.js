72be6f120e9653a583521620257054d5ec1d5bd1
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-06-06 20:36:22 +0200

    use Object.defineProperty

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var methods = require('./lib/methods');
 
 module.exports = (function() {
   Object.keys(methods).forEach(function(method) {
-    Array.prototype[method] = Array.prototype[method] || methods[method];
+    Object.defineProperty(Array.prototype, method, {
+      enumerable: false,
+      get: methods[method]
+    });
   });
 })();
