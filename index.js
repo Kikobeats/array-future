b20fd6020a108c49e65b5aa54f2ceb0e09e5aafb
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-06-06 20:52:31 +0200

    better initialization based on prototypes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var methods = require('./lib/methods');
 
 module.exports = (function() {
   Object.keys(methods).forEach(function(method) {
-    Array.prototype[method] = Array.prototype[method] || methods[method];
+    if (!Array.prototype[method]) {
+      Object.defineProperty(Array.prototype, method, {
+        value: methods[method]
+      });
+    }
   });
 })();
